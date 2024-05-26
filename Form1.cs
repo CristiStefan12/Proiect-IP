525cbbda6c694b7104bdac048a411c4f78f588f9
Andrei Vieru <mixerandr3w001@gmail.com>
2024-05-26 23:32:32 +0300

    Add basic Ollama integration

--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+
+
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -51,5 +50,72 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private async void guna2Button1_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string url = "http://192.168.100.82:11434/api/generate";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(@"
+                {
+                  ""model"": ""llama3:8b-instruct-fp16"",
+                  ""prompt"": ""tomato sauce, cheese, parmesan"",
+
+                  ""format"": ""json"",
+                  ""stream"": false,
+                  ""options"": {
+                    ""num_predict"": -1
+                  },
+
+                  ""system"":  ""You are a master chef, an expert of food. Write 10 more ingredients commonly used in recipes with the ingredients provided. Respond only in JSON using this format. ```ts { ingredients: string[] } ```""
+                }
+            ", Encoding.UTF8, "application/json")
+            };
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            var jsonResponse = JsonSerializer.Deserialize<OllamaResponse>(responseBody);
+            var llmResponseJson = JsonSerializer.Deserialize<IngredientsResponse>(jsonResponse.response);
+
+            string a = "";
+
+            for (int i = 0; i < llmResponseJson.ingredients.Length; i++)
+            {
+                a += llmResponseJson.ingredients[i] + Environment.NewLine;
+            }
+
+            guna2TextBox1.Text = a;
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
+
+    class OllamaResponse
+    {
+        public string model { get; set; }
+        public string created_at { get; set; }
+        public string response { get; set; }
+        public bool done { get; set; }
+        public int[] context { get; set; }
+        //public int total_duration { get; set; }
+        //public int load_duration { get; set; }
+        //public int prompt_eval_count { get; set; }
+        //public int prompt_eval_duration { get; set; }
+        //public int eval_count { get; set; }
+        //public int eval_duration { get; set; }
+    }
+
+    class IngredientsResponse
+    {
+        public string[] ingredients { get; set; }
+    }
+
 }
